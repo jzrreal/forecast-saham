4757e96d9d47447cda49fb5be55a41be47b136ae
Jezriel Lukas L <jezriellukas@gmail.com>
2024-10-11 13:25:25 +0700

    init

---
+++ src/main/java/com/adj/forecast/saham/ForecastSahamApplication.java
@@ -0,0 +1,13 @@
+package com.adj.forecast.saham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastSahamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForecastSahamApplication.class, args);
+    }
+    
+}
